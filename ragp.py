2fe1caf71ff75516f46da50a1aad01a1695be143
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-02-03 02:09:47 +0530

    changed cell dimensionst

--- ragp.py
+++ ragp.py
@@ -1,8 +1,13 @@
 from neuron import h
 from neuron.units import ms, mV
 
+h.load_file('stdrun.hoc')
+h.v_init = -65*mV
+
 soma = h.Section(name='soma')
-soma.L, soma.diam, soma.cm = 12.6157, 12.6157, 1
+soma.L, soma.diam, soma.cm = 30, 30, 1
+
+soma.insert('pas')
 soma.insert('hh')
 #hh.gnabar, hh.gkbar, hh.gl, hh.el = 0.12 , 0.036 , 0.0003 , -54.3 #NOTE: hh. throws an error
 soma(0.5).hh.gnabar = 0.12  # changing from parameter value in mod file
@@ -15,18 +20,16 @@ soma(0.5).hh.el = -54.3
 #    seg.hh.gnabar
 # soma.nseg = 3
 
-mech = soma(0.5).hh
-#print(dir(mech))
-
 ## INSERT AN ICLAMP/STIMULUS
 iclamp = h.IClamp(soma(0.5))
-
-#looking at iclamp params:
-print([item for item in dir(iclamp) if not item.startswith('__')])
-#['amp', 'baseattr', 'delay', 'dur', 'get_loc', 'get_segment', 'has_loc', 'hname', 'hocobjptr', 'i', 'loc', 'same']
 iclamp.delay = 2 #ms
 iclamp.dur = 0.1 #ms
 iclamp.amp = 0.9 #nA
+
+#looking at iclamp params:
+#print([item for item in dir(iclamp) if not item.startswith('__')])
+#['amp', 'baseattr', 'delay', 'dur', 'get_loc', 'get_segment', 'has_loc', 'hname', 'hocobjptr', 'i', 'loc', 'same']
+
 
 # get representation of current model
 soma.psection()
@@ -36,9 +39,8 @@ v = h.Vector().record(soma(0.5)._ref_v)             # membrane potential vector
 t = h.Vector().record(h._ref_t)                     # timestamp vector
 
 ## RUN SIMULATION
-h.load_file('stdrun.hoc') ## 
 # initalize sim w/ resting potential
-h.finitialize(-65 * mV)
+h.finitialize(h.v_init)
 # continue sim thru 40 ms
 h.continuerun(40 * ms)
 
@@ -52,15 +54,3 @@ plt.ylabel('v (mV)')
 plt.show()
 # SAVE figure
 
-
-## MISC CODE
-#print(mech.gkbar)
-#print(mech.gnabar)
-#print(mech.gl)
-#print(mech.el)
-#print(soma(0.5).hh.gkbar)
-
-#print("type(soma) = {}".format(type(soma)))
-#print("type(soma(0.5)) = {}".format(type(soma(0.5))))
-
-## inserting IClamp
