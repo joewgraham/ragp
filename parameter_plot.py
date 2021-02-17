3016a45ed03f8591c16af30db27d0f2c0f76c9e9
Jessica Feldman <jessfeld@gmail.com>
2021-02-16 21:27:12 -0500

    new version of parameter_plot.py

--- parameter_plot.py
+++ parameter_plot.py
@@ -6,10 +6,10 @@ Remember to assign the voltage output from the NEURON simulation in line 12
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from neuron import h
+
 
 ######## ASSIGN YOUR VOLTAGE VARIABLE HERE ########
-V = v # voltage output of simulation, or look up in Hodgkin and Huxley 1952 
+V = v # voltage output of simulation
 
 # Fast Sodium Parameters
 m_inf_Na = (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5))) / (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5)) + 0.062 * (V + 38) / (np.exp((V + 38)/5)-1))
@@ -19,6 +19,7 @@ tau_hNa = 1 / (0.016 * np.exp(-(V + 55) / 15) + 2.07 / (1 + np.exp(-(V - 17) / 2
 
 # Potassium-delayed rectifier
 m_inf_DR = (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5))) / (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
+tau_m_DR = 1/(0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
 
 # Plot
 fig, axs = plt.subplots(5) #, sharex=True, sharey=True)
@@ -31,7 +32,7 @@ fig.suptitle('tau_mNa')
 axs[1].set(ylabel='tau_mNA (1/mV)')
 
 axs[2].plot(t, h_inf_Na)
-fig.suptitle('≈≈')
+fig.suptitle('h_inf_Na')
 axs[2].set(ylabel='h_inf_Na (unitless)')
 
 axs[3].plot(t, tau_hNa)
@@ -42,12 +43,9 @@ axs[4].plot(t, m_inf_DR)
 fig.suptitle('m_inf_DR')
 axs[4].set(ylabel='m_inf_DR (unitless)')
 
+axs[5].plot(t, tau_m_DR)
+fig.suptitle('tau_m_DR')
+axs[5].set(ylabel='tau_m_DR (1/mV)')
+
 plt.xlabel('t (ms)')
 plt.show()
-
-from scipy.io import savemat
-import json
-
-### ADD SAVE TO FILE for above m, h PARAMS ###
-# mat file 
-# json file
