ef7cb8b7c9104319406d1ec700f4cb78e14a98e3
Jessica Feldman <jessfeld@gmail.com>
2021-02-23 20:21:38 -0500

    mat file

--- parameter_plot.py
+++ parameter_plot.py
@@ -16,27 +16,38 @@ v =  sim.allSimData['V_soma']['cell_0']
 #v = h.Vector().record(soma(0.5)._ref_v)             # membrane potential vector
 #t = h.Vector().record(h._ref_t)                     # timestamp vector
 ######## ASSIGN YOUR VOLTAGE VARIABLE HERE ########
-V = v # voltage output of simulation
+#V = v # voltage output of simulation
 
 # Fast Sodium Parameters
-m_inf_Na = (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5))) / (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5)) + 0.062 * (V + 38) / (np.exp((V + 38)/5)-1))
-tau_mNa = 1 / (0.091 * (V + 38) / (1 - np.exp(-(V + 38) / 5)) + 0.062 * (V + 38) / (np.exp((V + 38) / 5) - 1))
-h_inf_Na = (0.016 * np.exp(-(V + 55) / 15)) / (0.016 * np.exp(-(V + 55) / 15) + 2.07 / (1 + np.exp(-(V - 17) / 21)))
-tau_hNa = 1 / (0.016 * np.exp(-(V + 55) / 15) + 2.07 / (1 + np.exp(-(V - 17) / 21)))
-
+## V+38
+## V+ 55
+## V-17
+i=0
+while i < len(v):
+    V= v[i] + 38
+    V = v[i]
+    m_inf_Na = (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5))) / (0.091 * (V + 38) / (1-np.exp(-(V + 38)/5)) + 0.062 * (V + 38) / (np.exp((V + 38)/5)-1))   
+    tau_mNa = 1 / (0.091 * (V + 38) / (1 - np.exp(-(V + 38) / 5)) + 0.062 * (V + 38) / (np.exp((V + 38) / 5) - 1))
+    h_inf_Na = (0.016 * np.exp(-(V + 55) / 15)) / (0.016 * np.exp(-(V + 55) / 15) + 2.07 / (1 + np.exp(-(V - 17) / 21)))
+    tau_hNa = 1 / (0.016 * np.exp(-(V + 55) / 15) + 2.07 / (1 + np.exp(-(V - 17) / 21)))
+  
 # Potassium-delayed rectifier
-m_inf_DR = (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5))) / (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
-tau_m_DR = 1/(0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
+    m_inf_DR = (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5))) / (0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
+    tau_m_DR = 1/(0.01 * (V + 45) / (1 - np.exp(-(V + 45) / 5)) + 0.17 * np.exp(-(V + 50) / 40))
 
+     i=i+1
 # Plot
 fig, axs = plt.subplots(5) #, sharex=True, sharey=True)
 axs[0].plot(t, m_inf_Na)    # Insert time vector used here
 fig.suptitle('m_inf_Na')
 axs[0].set(ylabel='m_inf_Na (unitless)')
 
+    
 axs[1].plot(t, tau_mNa)
 fig.suptitle('tau_mNa')
 axs[1].set(ylabel='tau_mNA (1/mV)')
+
+    
 
 axs[2].plot(t, h_inf_Na)
 fig.suptitle('h_inf_Na')
@@ -52,7 +63,9 @@ axs[4].set(ylabel='m_inf_DR (unitless)')
 
 axs[5].plot(t, tau_m_DR)
 fig.suptitle('tau_m_DR')
-axs[5].set(ylabel='tau_m_DR (1/mV)')
+    axs[5].set(ylabel='tau_m_DR (1/mV)')
 
 plt.xlabel('t (ms)')
+
+   
 plt.show()
