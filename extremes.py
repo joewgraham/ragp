9452bd2cb40beba00d512ddaf495337ddc4bce39
jessicafeldman <jessfeld@gmail.com>
2021-03-07 14:25:13 -0500

    Update extremes.py

--- extremes.py
+++ extremes.py
@@ -50,8 +50,9 @@ soma(0.5).ch_Hcn4_cp12.gHCN4bar = 0.001 #0.01 adjusted for spiking
 #### ICLAMP ####
 iclamp = h.IClamp(soma(0.5))
 iclamp.delay = 50 #ms
-iclamp.dur = 0.5 #ms
-iclamp.amp = 2 #nA
+iclamp.dur = 0.5 #ms  #200 if squarewave
+#istop = 500 # if squarewave
+iclamp.amp = 0.05 #nA
 
 
 v = h.Vector().record(soma(0.5)._ref_v)             # membrane potential vector
