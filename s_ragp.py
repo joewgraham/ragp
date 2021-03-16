1f11445f80925a8a744ba587db61142d5808018c
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-03-17 01:59:35 +0530

    updating s_ragp.py

--- s_ragp.py
+++ s_ragp.py
@@ -7,6 +7,7 @@ h.v_init = -65*mV
 soma = h.Section(name='soma')
 soma.L, soma.diam, soma.cm = 30, 30, 1
 
+<<<<<<< HEAD:s_ragp.py
 #soma.insert('pas')
 #soma.insert('hh')
 #hh.gnabar, hh.gkbar, hh.gl, hh.el = 0.12 , 0.036 , 0.0003 , -54.3 #NOTE: hh. throws an error
@@ -14,12 +15,22 @@ soma.L, soma.diam, soma.cm = 30, 30, 1
 # soma(0.5).hh.gkbar # = 0.036
 # soma(0.5).hh.gl #= 0.0003
 # soma(0.5).hh.el #= -54.3
+=======
+soma.insert('pas')
+soma.insert('hh')
+#hh.gnabar, hh.gkbar, hh.gl, hh.el = 0.12 , 0.036 , 0.0003 , -54.3 #NOTE: hh. throws an error
+soma(0.5).hh.gnabar = 0.12  # changing from parameter value in mod file
+soma(0.5).hh.gkbar  = 0.036 #  
+soma(0.5).hh.gl = 0.0003
+soma(0.5).hh.el = -54.3
+>>>>>>> 744e5b9fca5505a5ec9e19dcbfba1d15e41b4b09:ragp.py
 
 # SET NSEG - setting hh.gnabar for all nseg
 # for seg in soma: print(seg)
 #    seg.hh.gnabar
 # soma.nseg = 3
 
+<<<<<<< HEAD:s_ragp.py
 # mech = soma(0.5).hh
 #print(dir(mech))
 
@@ -32,6 +43,18 @@ soma.L, soma.diam, soma.cm = 30, 30, 1
 # iclamp.delay = 2 #ms
 # iclamp.dur = 0.1 #ms
 # iclamp.amp = 0.9 #nA
+=======
+## INSERT AN ICLAMP/STIMULUS
+iclamp = h.IClamp(soma(0.5))
+iclamp.delay = 2 #ms
+iclamp.dur = 0.1 #ms
+iclamp.amp = 0.9 #nA
+>>>>>>> 744e5b9fca5505a5ec9e19dcbfba1d15e41b4b09:ragp.py
+
+#looking at iclamp params:
+#print([item for item in dir(iclamp) if not item.startswith('__')])
+#['amp', 'baseattr', 'delay', 'dur', 'get_loc', 'get_segment', 'has_loc', 'hname', 'hocobjptr', 'i', 'loc', 'same']
+
 
 # get representation of current model
 soma.psection()
@@ -56,15 +79,3 @@ plt.ylabel('v (mV)')
 plt.show()
 # SAVE figure
 
-
-## MISC CODE
-#print(mech.gkbar)
-#print(mech.gnabar)
-#print(mech.gl)
-#print(mech.el)
-#print(soma(0.5).hh.gkbar)
-
-#print("type(soma) = {}".format(type(soma)))
-#print("type(soma(0.5)) = {}".format(type(soma(0.5))))
-
-## inserting IClamp
