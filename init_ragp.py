98df00504255b86b37d78137c87766c9dc81963c
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-03-18 00:11:40 +0530

    Porting NEURON's RAGP model to NetPyNE

---
+++ init_ragp.py
@@ -0,0 +1,23 @@
+from netpyne import specs, sim
+
+# Create network and run simulation
+from cfg import cfg
+from netParams import netParams
+import matplotlib.pyplot as plt
+plt.ion()
+
+
+print("Starting sim ...")
+sim.createSimulateAnalyze(netParams, cfg)
+sim.createSimulateAnalyze(netParams=netParams, simConfig=cfg)
+print("Finished sim.")  
+
+# fig_dict, data_dict = sim.analysis.plotTraces() #ylim=[-80,60]
+# fig0 = fig_dict['_gid_0']
+# for ax in fig0.axes:
+#     ax.set_xlabel('Time (ms)')
+#     ax.set_ylabel('Membrane Voltage (mV)')
+#     ax.set_title('')
+# fig0.savefig('AP.png')
+   
+
