645163642c8d0da9e5fa4804bb8e9f24ad14409c
Jessica Feldman <jessfeld@gmail.com>
2021-04-30 18:31:32 -0400

    added newgenemods

--- netParams.py
+++ netParams.py
@@ -8,17 +8,30 @@ from cfg import cfg
 PYRcell = {'secs': {}}
 
 PYRcell['secs']['soma'] = {'geom': {}, 'mechs': {}}  
-PYRcell['secs']['soma']['geom'] = {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm': 1} #123.0
+PYRcell['secs']['soma']['geom'] = {'diam': 30, 'L': 30, 'Ra': 35.4, 'cm': 1} #123.0 <--- why did Ra change?
 
 PYRcell['secs']['soma']['mechs']['pas'] = {'g': 1.8e-6, 'e': -65} 	
+#Ca
+PYRcell['secs']['soma']['mechs']['ch_Cacna1a_cp5.mod'] = {'': 0.0001} 
+PYRcell['secs']['soma']['mechs']['ch_Cacna1g_cp41'] = {'': 0.0001} 
+
 PYRcell['secs']['soma']['mechs']['ch_Cacna1b_cp6'] = {'gCav2_2bar': 0.0001} 
-PYRcell['secs']['soma']['mechs']['ch_Cacna1c_cp3'] = {'gLbar': 0.0001} 		
-PYRcell['secs']['soma']['mechs']['ch_Cacna1i_cp42'] = {'gCav3_3bar': 0.0001} 		
-PYRcell['secs']['soma']['mechs']['ch_Scn1a_md264834'] = {'gNav11bar': 2.0} 	# 0.00001
-PYRcell['secs']['soma']['mechs']['ch_Hcn2_cp10'] = {'gHCN2bar': 0.010}		
-PYRcell['secs']['soma']['mechs']['ch_Hcn4_cp12'] = {'gHCN4bar': 0.001}		
-PYRcell['secs']['soma']['mechs']['ch_Kcnc1_md74298'] = {'gk': 0.015} 		
+PYRcell['secs']['soma']['mechs']['ch_Cacna1c_cp3'] = {'gLbar': 0.0001} 
+PYRcell['secs']['soma']['mechs']['ch_Cacna1i_cp42'] = {'gCav3_3bar': 0.0001} 
+
+#Na
+PYRcell['secs']['soma']['mechs']['ch_Scn1a_md264834'] = {'gNav11bar': 1.0} 	# 0.00001
+
+#K		
+PYRcell['secs']['soma']['mechs']['ch_Kcnc1_md74298'] = {'gk': 0.1} #changed to 0.1  		
 PYRcell['secs']['soma']['mechs']['ch_Kcna1ab1_md80769'] = {'gbar': 0.015} 
+
+#Hcn
+PYRcell['secs']['soma']['mechs']['ch_Hcn1_cp9'] = {'':}
+PYRcell['secs']['soma']['mechs']['ch_Hcn3_cp11'] = {'':}
+
+PYRcell['secs']['soma']['mechs']['ch_Hcn2_cp10'] = {'gHCN2bar': 0.010} #	
+PYRcell['secs']['soma']['mechs']['ch_Hcn4_cp12'] = {'gHCN4bar': 0.001} #
 
 # PYRcell['secs']['soma']['ions']['k']['e'] = -74
 PYRcell['secs']['soma']['ions'] = {'k': {'e': -74}}
@@ -39,7 +52,6 @@ for secName in PYRcell['secs']:
 netParams.popParams['ragp'] = {'cellType': 'PYR', 'numCells': 1}
 
 # Stimulation parameters
-#{'type': 'IClamp', 'amp': 0, 'dur': 0, 'delay': 1}
 netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': 0.1, 'dur': 200, 'delay': 200} #amp 0.1; del 100; dur 200
 netParams.stimTargetParams['iclamp->PYR'] = {'source': 'iclamp', 'conds': {'cellType': 'PYR'}, 'sec': 'soma', 'loc': 0.5}
 
