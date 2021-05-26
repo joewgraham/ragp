481efb205340d64cd4b9b28fa71b83dd61d4d1c4
Jessica Feldman <jessfeld@gmail.com>
2021-05-26 12:25:26 -0400

    updating numpy to np

--- netParams.py
+++ netParams.py
@@ -1,7 +1,6 @@
 from netpyne import specs, sim
 import scipy.io as sio 
-import array
-import numpy
+import numpy as np
 import csv
 
 try:
@@ -15,10 +14,10 @@ netParams = specs.NetParams()  # object of class NetParams to store the network 
 ## LOAD ALL CELL TYPES
 # from mat file
 cellnum = 53
-cell_identities = numpy.transpose(sio.loadmat('all_cellTypes.mat')['allcells_new12_unique_binary'])
+cell_identities = np.transpose(sio.loadmat('all_cellTypes.mat')['allcells_new12_unique_binary'])
 cell = cell_identities[cellnum]
 #from csv
-cell_identities = numpy.int_(numpy.transpose(numpy.genfromtxt('allcells_new12_unique_binary.csv', delimiter=',')))
+cell_identities = np.int_(np.transpose(np.genfromtxt('allcells_new12_unique_binary.csv', delimiter=',')))
 
 # order in genemod MUST be preserved to match cell_identities channel order
 genemod = {'Cacna1a':('ch_Cacna1a_cp5', {'gCav2_1bar': 0.00001}),  'Cacna1b':('ch_Cacna1b_cp6', {'gCav2_2bar': 0.0001}),
