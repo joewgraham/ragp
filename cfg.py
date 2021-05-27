46ada10988523c15c98108335c1d2c52eb613b21
Bill Lytton <neuro014@gmail.com>
2021-05-27 17:18:29 -0400

    just check 3 cells

--- cfg.py
+++ cfg.py
@@ -2,7 +2,6 @@ from netpyne import specs
 cfg = specs.SimConfig()     # object of class SimConfig to store simulation configuration
 
 cfg.duration = 5 # 1*1e3        # Duration of the simulation, in ms
-cfg.tstop = 500
 cfg.dt = 0.025              # Internal integration timestep to use
 cfg.verbose = False         # Show detailed messages
 cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
