0dc306b5886af22a8d3df4383032e8e914daadca
Bill Lytton <neuro014@gmail.com>
2021-05-27 17:56:05 -0400

    single line init

--- init.py
+++ init.py
@@ -1,7 +1,4 @@
 from netpyne import sim
 
 simConfig, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
-sim.create()
-sim.simulate()
-sim.analyze()
-
+sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)
