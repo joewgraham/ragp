4f8290e00bdfff6ba46b0bec1b0ce8dd5c4149fa
Suranjana Gupta <gupta8suranjana@gmail.com>
2021-05-28 12:46:33 -0400

    changed batchlabel

--- batch.py
+++ batch.py
@@ -7,7 +7,7 @@ def batch():
         params['cellnum'] = [x for x in range(0,2)]
         b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py',)
         # Set output folder, grid method (all param combinations), and run configuration
-        b.batchLabel = '21may27a'
+        b.batchLabel = '21may28a'
         b.saveFolder = 'data'
         b.method = 'grid'
         b.runCfg = {'type': 'mpi_bulletin', 'script': 'init.py', 'skip': True}
